38c7fc14f30906cb12cbb20ac3f24afe59b2506f
Vanessa <vanessamrios@gmail.com>
2022-04-12 12:15:57 +0200

    (Req 01) Cria a classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  private static raceCount = 0;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    Race.createdRacesInstances();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
